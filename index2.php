e1b0cd375e1ab8a7bf4a890edd08ee4ce836030d
Dominik <163560221+Dominik-developer@users.noreply.github.com>
2024-03-19 20:20:45 +0100

    Update and rename totalizator2.php to index2.php

--- index2.php
+++ index2.php
@@ -7,7 +7,7 @@
     <meta name="keywords" content="lotto" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
     <meta name="author" content="Dominik Szczepański" />
-    <link rel="stylesheet"href="totalizator.css" type="text/css" />
+    <link rel="stylesheet"href="index.css" type="text/css" />
 </head>
 
 
@@ -20,8 +20,8 @@
     </div>
  
     <div id="menu">
-        <div class="option"><a href="totalizator.php"> Strona główna</a></div>
-        <div class="option"><a href="wiek.php"> Lotto</a></div>
+        <div class="option"><a href="index.php"> Strona główna</a></div>
+        <div class="option"><a href="agephp"> Lotto</a></div>
         <div class="option"><a href="#"> O Autorze</a></div>
         <div class="option"><a href="#"> Podziękowania</a></div>
         
@@ -34,7 +34,7 @@
     </div>
 
     <div id="sidebar">
-        <div class="optionL"><a href="totalizator.php">Graj </a> </div> 
+        <div class="optionL"><a href="index.php">Graj </a> </div> 
         <div class="optionL">Zasady </div>
         <div class="optionL">O grze  </div>
     </div>
@@ -46,7 +46,7 @@
 
 <div>
     <p>Lotto</p>
-    <form method="post" action="Wynik.php">
+    <form method="post" action="result.php">
     <span>Podaj 6 liczb (od 1 do 49), oddzielając je przecinkiem:</span>
     <input type="text" name="liczby" required><br>
     <input type="submit" value="Wyślij">
